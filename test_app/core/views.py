b61379a7d465256c52453cd8b364a4f53e5858aa
Ilya Sheroukhov <sheroukhov@gmail.com>
2019-11-08 22:29:43 +0300

    Clean build_no from html

--- test_app/core/views.py
+++ test_app/core/views.py
@@ -7,7 +7,5 @@ core = Blueprint('core', __name__, template_folder='templates/core')
 @core.route('/')
 def index():
     time = str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-    build_number = str(os.environ[BUILD_NUMBER])
     return render_template('index.html',
-                                time=time,
-                                build_no=build_number)
+                                time=time)
