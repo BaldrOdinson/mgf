18554053aadab7dd2854d2398eed9e9d4773e81f
Ilya Sheroukhov <sheroukhov@gmail.com>
2019-11-08 22:08:33 +0300

    add build data to html

--- test_app/core/views.py
+++ test_app/core/views.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, request, redirect, url_for, Blueprint
 from time import gmtime, strftime
 
@@ -6,5 +7,7 @@ core = Blueprint('core', __name__, template_folder='templates/core')
 @core.route('/')
 def index():
     time = str(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    build_number = str(os.environ[BUILD_NUMBER])
     return render_template('index.html',
-                                time=time)
+                                time=time,
+                                build_no=build_number)
